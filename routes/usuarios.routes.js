ffdb858b7661bfad93e992ef82940d0440f73a18
Hazael Lopez <elhazael134@gmail.com>
2022-08-01 20:42:01 -0600

    Tarea - Custom validation - EmailExiste

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { esRolValido } = require('../helpers/db-validators');
+const { esRolValido, emailExiste } = require('../helpers/db-validators');
 const { validarCampos } = require('../middlewares/validar-campos');
 const { 
         usuariosGet, 
@@ -19,6 +19,7 @@ router.post('/',
           check('nombre','El nombre es obligatorio').not().isEmpty(),
           check('password','El password debe tener de 6 a más caracteres').isLength({ min: 6 }),
           check('correo','El correo no es valido').isEmail(),
+          check('correo').custom(emailExiste),
         //check('rol', 'No es un rol permitido').isIn(['ADMIN_ROLE','USER_ROLE']),
           check('rol').custom(esRolValido),
           validarCampos
