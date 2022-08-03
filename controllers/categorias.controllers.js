d1a2f24a9c4dac508533766a6e957047cb062b79
Hazael Lopez <elhazael134@gmail.com>
2022-08-03 17:20:37 -0600

    Crear una categoria

--- controllers/categorias.controllers.js
+++ controllers/categorias.controllers.js
@@ -1,26 +1,54 @@
+const { response } = require('express');
+const { Categoria } = require('../models');
 
 (req, res = response) => {
     res.status(200).json({
         msg: 'Categorias get'
     });
 }
+
 (req, res = response) => {
     res.status(200).json({
         msg: 'Categorias get id'
     });
 }
-(req, res = response) => {
-    res.status(200).json({
-        msg: 'Categorias post '
-    });
+
+const crearCategoria = async (req, res = response) => {
+
+    const nombre  = req.body.nombre.toUpperCase();
+    const categoriaDB = await Categoria.findOne({ nombre });
+
+    if(categoriaDB){
+        return res.status(400).json({
+            msg: `La categoria ${categoriaDB.nombre} ya existe`
+        });
+    }
+
+    // Generar la Data a Guardar
+    const data  = {
+        nombre,
+        usuario: req.usuario._id
+    }
+    
+    const categoria = await Categoria(data);
+    // Guardar DB
+    await categoria.save();
+
+    res.status(201).json(categoria);
 }
+
 (req, res = response) => {
     res.status(200).json({
         msg: 'Categorias put id'
     });
 }
+
 (req, res = response) => {
     res.status(200).json({
         msg: 'Categorias delete id'
     });
+}
+
+module.exports = {
+    crearCategoria
 }
--- routes/categorias.routes.js
+++ routes/categorias.routes.js
@@ -1,44 +1,32 @@
-const { response } = require('express');
 const { Router } = require('express');
 const { check } = require('express-validator');
+const { validarCampos, validarJWT } = require('../middlewares');
 
-const { validarCampos } = require('../middlewares/validar-campos');
+const { crearCategoria } = require('../controllers/categorias.controllers');
 
 const router = Router();
 
 // Obtener todas la categorias - Servicio público
-router.get('/', (req, res = response) => {
-    res.status(200).json({
-        msg: 'Categorias get'
-    });
-});
+router.get('/', );
 
 // Obtener Categoria por Id -Servicio público
-router.get('/:id', (req, res = response) => {
-    res.status(200).json({
-        msg: 'Categorias get id'
-    });
-});
+router.get('/:id', );
 
 // Crear una categoria - Servicio privado jwt
-router.post('/', (req, res = response) => {
-    res.status(200).json({
-        msg: 'Categorias post '
-    });
-});
+router.post(
+    '/', 
+    [
+      validarJWT,
+      check('nombre','El nombre es oblicatorio').not().isEmpty(),
+      validarCampos
+    ], 
+    crearCategoria
+);
 
 // Actualizar un registro por Id -Servicio Privado jwt
-router.put('/:id', (req, res = response) => {
-    res.status(200).json({
-        msg: 'Categorias put id'
-    });
-});
+router.put('/:id', );
 
 // Borrar una categoria - Servicio privado 
-router.delete('/:id', (req, res = response) => {
-    res.status(200).json({
-        msg: 'Categorias delete id'
-    });
-});
+router.delete('/:id', );
 
 module.exports = router;
