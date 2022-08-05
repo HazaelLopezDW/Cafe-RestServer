6eaceaca388b13727d221caf5ad8939e704df9df
Hazael Lopez <elhazael134@gmail.com>
2022-08-05 15:30:13 -0600

    Borrar archivos del servidor

--- controllers/uploads.controllers.js
+++ controllers/uploads.controllers.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const fs = require('fs');
+
 const { response } = require("express");
 const { subirArchivo } = require("../helpers");
 const { Usuario, Producto } = require('../models')
@@ -45,6 +48,15 @@ const actualizarImagen = async (req, res = response) => {
             return res.status(500).json({
                 msg: `Se me olvidó validar esto`
             });
+    }
+
+    // limpiar imágenes previas
+    if(modelo.img){
+        // Hay que borrar la imagen del servidor
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+        if(fs.existsSync(pathImagen)){
+            fs.unlinkSync(pathImagen);
+        }
     }
 
     const nombre = await subirArchivo(req.files, undefined, coleccion);
